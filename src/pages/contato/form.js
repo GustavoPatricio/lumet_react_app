94110b47cb3f458536fb75de1748cb246cb27345
Gustavo Patrício <gustavopatricio11@gmail.com>
2020-06-13 01:49:50 -0300

    Validação dos campos da página: Contato (react-hook-forms)

--- src/pages/contato/form.js
+++ src/pages/contato/form.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import InputMask from "react-input-mask";
+import './form.css';
+//O style do form está no contato.css
 
 function Form() { 
    const {register, handleSubmit, errors } = useForm();
@@ -9,29 +12,41 @@ function Form() {
    }
 
    return (
-    <form onSubmit={handleSubmit(onSubmit)} data-aos="fade-up" autoComplete="off">
+    <form onSubmit={handleSubmit(onSubmit)} data-aos="fade-up" autoComplete='off'>
         <div className="row">
+            
             <input type="text" name="nome" id="nome" ref={register({ required: true, maxLength: 50 })} placeholder="Seu nome"/>
-            {errors.nome && errors.nome.type === "required" && <span style={{color: 'white !important'}}>teste</span>}
+            {errors.nome && errors.nome.type === "required" && <div style={{color: '#ff4f40', bottom: '0', fontSize: '.85rem'}}>Este campo é necessário</div>}
+            {errors.nome && errors.nome.type === "maxLength" && <div style={{color: '#ff4f40', bottom: '0', fontSize: '.85rem'}}>Tamanho máximo do campo excedido (max: 50 caracteres)</div>}
         </div>
         <div className="row">
-            <input type="email" name="Email" id="Email"  placeholder="Seu email" required/>
+            <input type="email" name="email" id="email" ref={register({ required: true, maxLength: 50 })}  placeholder="Seu email" autoComplete='off'/>
+            {errors.email && errors.email.type === "required" && <div style={{color: '#ff4f40', bottom: '0', fontSize: '.85rem'}}>Este campo é necessário</div>}
+            {errors.email && errors.email.type === "maxLength" && <div style={{color: '#ff4f40', bottom: '0', fontSize:  '.85rem'}}>Tamanho máximo do campo excedido (max: 50 caracteres)</div>}
         </div>
         <div className="row">
-            <input type="text" name="Empresa" id="Empresa"  placeholder="Sua empresa (opcional)"/>
+            <input type="text" name="empresa" id="empresa" ref={register({ maxLength: 50 })} placeholder="Sua empresa (opcional)"/>
+            {errors.empresa && errors.empresa.type === "maxLength" && <div style={{color: '#ff4f40', bottom: '0', fontSize:  '.85rem'}}>Tamanho máximo do campo excedido (max: 50 caracteres)</div>}
         </div>
         <div className="row">
-            <input type="tel" name="Telefone" id="Telefone"  placeholder="Telefone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
+            <InputMask mask="(99) 99999-9999" type="text" name="telefone" id="telefone" ref={register({ required: true, minLength: 15, maxLength: 15})} placeholder="Telefone"/>
+            {errors.Telefone && errors.Telefone.type === "required" && <div style={{color: '#ff4f40', bottom: '0', fontSize: '.85rem'}}>Este campo é necessário</div>}
+            {errors.Telefone && (errors.Telefone.type === "minLength" || errors.Telefone.type === "maxLength") && <div style={{color: '#ff4f40', bottom: '0', fontSize:  '.85rem'}}>Contato inválido</div>}
         </div>
         <div className="row">
-            <input type="text" name="Assunto" id="Assunto"  placeholder="Assunto" required/>
+            <input type="text" name="assunto" id="assunto" ref={register({ required: true, maxLength: 50 })} placeholder="Assunto"/>
+            {errors.assunto && errors.assunto.type === "required" && <div style={{color: '#ff4f40', bottom: '0', fontSize: '.85rem'}}>Este campo é necessário</div>}
+            {errors.assunto && errors.assunto.type === "maxLength" && <div style={{color: '#ff4f40', bottom: '0', fontSize:  '.85rem'}}>Tamanho máximo do campo excedido (max: 50 caracteres)</div>}
         </div>
         <div className="row">
-            <textarea name="Mensagem" id="Mensagem" placeholder="Mensagem"></textarea>
+            <textarea name="mensagem" id="mensagem" ref={register({ required: true, maxLength: 1000 })} placeholder="mensagem"></textarea>
+            {errors.assunto && errors.assunto.type === "required" && <div style={{color: '#ff4f40', bottom: '0', fontSize: '.85rem'}}>Este campo é necessário</div>}
+            {errors.assunto && errors.assunto.type === "maxLength" && <div style={{color: '#ff4f40', bottom: '0', fontSize:  '.85rem'}}>Tamanho máximo do campo excedido (max: 1000 caracteres)</div>}
         </div>
         <div className="row" style={{marginTop: '5vh'}}>
             <input type="submit" value="Enviar"/>
         </div> 
+        
         
     </form>
   );
